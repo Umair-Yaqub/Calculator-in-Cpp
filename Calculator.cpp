0a6ddf85ed7cccec06379b7276a53e5805784733
Umair Yaqub <umairyaqubofficial@gmail.com>
2022-01-04 13:05:31 +0500

    Added comments.

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,40 +5,52 @@ char Operator ;
 
 int main (){
 
+    // It allow user to enter the operand
     std :: cout << "You'll need to enter the 1st number :: " ;
     std :: cin >> Number_1 ;
 
+    // It allows user to enter operator i.e. +, -, *, /
     std :: cout << "You'll need to enter the operator :: " ;
     std :: cin >> Operator ;
 
+    // It allow user to enter the operand
     std :: cout << "You'll need to enter the 2nd number :: " ;
     std :: cin >> Number_2 ;
 
+    // Switch statement begins
     switch ( Operator ){
 
+    // If user enter /
     case '/' :
         
         std :: cout << "Calculations :: " << Number_1 / Number_2 ;
         break ;
 
+    // If user enter *
     case '*' :
         
         std :: cout << "Calculations :: " << Number_1 * Number_2 ;
         break ;
 
+    // If user enter +
     case '+' :
         
         std :: cout << "Calculations :: " << Number_1 + Number_2 ;
         break ;
 
+    // If user enter -
     case '-' :
         
         std :: cout << "Calculations :: " << Number_1 - Number_2 ;
         break ;
-    
+        
+    // If the operator is other than +, -, * or /, 
+    // error message will display
     default :
     
         std :: cout << "Error!\nIncorrect Input." ;
         break ;
     }
+    // switch statement ends
+    
 }
